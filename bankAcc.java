10072c5e1f9e10699214a37c59d3030d882e888d
Nolan Kohler <kohlern@wlha.wlwv.k12.or.us>
2019-11-07 09:55:14 -0800

    cool

--- bankAcc.java
+++ bankAcc.java
@@ -1,32 +1,18 @@
-public class Bank{
-  private int accNum;
+public class bankAcc{
+  private String accNum;
   private String name;
-  private String email;
-  private String pass;
   private double money;
-  public Bank(int newAccNum, String newName, String newEmail, String newPass, double newMoney){
+  public Bank(String newAccNum, String newName, double newMoney){
     accNum = newAccNum;
     name = newName;
-    email = newEmail;
-    pass = newPass;
     money = newMoney;
   }
   public String toString(){
     String result = "";
-    //result += "Account number: " + accNum + "\n";
-    result += "Checking account: " + 
-  }
-  public int getAccNum(){
-    return accNum;
+    result += "Checking account: " +
   }
   public String getName(){
     return name;
-  }
-  public String getEmail(){
-    return email;
-  }
-  public String getPass(){
-    return pass;
   }
   public double getMoney(){
     return money;
---
+++ bankAccount.java
@@ -0,0 +1,8 @@
+public class bankAccount{
+  private String accNum = "";
+  private String name = "";
+  private String pinNum = "";
+  private double checkingMon;
+  private double savingMon;
+  
+}
